606b00f726282fe408dd970bfc56e8bce289303e
Ivan <Zolotukhin.ivan.1@gmail.com>
2018-11-07 22:17:57 +0200

    Added bot

---
+++ index.js
@@ -0,0 +1,23 @@
+// MicroBot creator Zolotukhin Ivan (7.11.18)
+
+let TelegramBot = require('./node_modules/node-telegram-bot-api');
+
+// Устанавливаем токен, который выдавал нам бот.
+let token = '775038949:AAFmuDD0ALsBz1Nug8t1A776lpW8teyxl-E';
+// Включить опрос сервера
+let bot = new TelegramBot(token, {polling: true});
+
+// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием.)
+bot.onText(/echo (.+)/, function (msg, match) {
+const fromId = msg.from.id;
+const resp = match[1];
+bot.sendMessage(fromId, resp);
+});
+
+// Простая команда без параметров.
+bot.on('message', function (msg) {
+    const chatId = msg.chat.id;
+    // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
+    const photo = 'cats.png';
+    bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
+});
