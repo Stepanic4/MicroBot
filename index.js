323948d69488f6913e3730fb230143f6cbc0cfed
Ivan <Zolotukhin.ivan.1@gmail.com>
2018-11-09 00:17:08 +0200

    added new dialog

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 // MicroBot creator Zolotukhin Ivan (7.11.18)
 
-let TelegramBot = require('./node_modules/node-telegram-bot-api');
 
-// Устанавливаем токен, который выдавал нам бот.
-let token = 'https://api.telegram.org/bot775038949:AAFmuDD0ALsBz1Nug8t1A776lpW8teyxl-E/getMe';
-// Включить опрос сервера
-let bot = new TelegramBot(token, {polling: true});
+const TelegramBot = require('node-telegram-bot-api');
 
-// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием.)
-bot.onText(/echo (.+)/, function (msg, match) {
-const fromId = msg.from.id;
-const resp = match[1];
-bot.sendMessage(fromId, resp);
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '685656590:AAEIB7YfCalkxVILZCEIr6ylANrtVRfECpc';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+const notes = [];
+
+bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
+    const userId = msg.from.id;
+    const text = match[1];
+    const time = match[2];
+
+    notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
 });
 
-// Простая команда без параметров.
-bot.on('message', function (msg) {
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
     const chatId = msg.chat.id;
-    // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
-    const photo = 'cats.png';
-    bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
+    const resp = match[1]; // the captured "whatever"
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
 });
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Приветствую тебя мой друг.');
+});
+
+
+
