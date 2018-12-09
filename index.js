1baf63a3afc6466ae4b36932a12b9d005eab0e01
Ivan <Zolotukhin.ivan.1@gmail.com>
2018-12-09 18:01:24 +0200

    change bot

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 // MicroBot creator Zolotukhin Ivan (7.11.18)
 
 const TelegramBot = require('node-telegram-bot-api');
-// replace the value below with the Telegram token you receive from @BotFather
+
 const token = '685656590:AAEIB7YfCalkxVILZCEIr6ylANrtVRfECpc';
-// Create a bot that uses 'polling' to fetch new updates
+
 const bot = new TelegramBot(token, {polling: true});
-const notes = [];
-// Matches "/echo [whatever]"
+
 bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
+
     const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-    // send back the matched "whatever" to the chat
+    const resp = match[1];
+
     bot.sendMessage(chatId, resp);
 });
-// Listen for any kind of message. There are different kinds of
-// messages.
+
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Приветствую тебя мой друг. ' +
-        'Я становлюсь каждый день умнее :)');
-});
-
-bot.onText(/\/напомни (.+) в (.+)/, function (msg, match) {
-    const userId = msg.from.id;
-    const text = match[1];
-    const time = match[2];
-    notes.push({ 'uid': userId, 'time': time, 'text': text });
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не забуду про тебя :)');
+    if (msg === 'Hello') {
+        bot.sendMessage(chatId, 'Приветик :)');
+    } else {
+        bot.sendMessage(chatId, 'Fuck')
+    }
 });
 
 
